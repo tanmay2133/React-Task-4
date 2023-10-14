ab336e37b8621c63b58d7e86275f3906c615979c
tanmay2133 <tanmaynaik2003@gmail.com>
2023-10-14 20:06:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [data,setdata]=useState({users:[]});
+  const url='https://dummyjson.com/users';
+  useEffect(()=>{
+    fetchdata();
+  },[]);
+
+  const fetchdata=()=>{
+    fetch(url)
+    .then((response) => response.json())
+    .then((data)=>{
+      setdata(data);
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Dummy Data</h1>
+        <table>
+        <thead>
+            <tr className='rl'>
+                <th>Sno</th>
+                <th>Profile Pic</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Gender</th>
+                <th>Email</th>
+                <th>UserName</th>
+                <th>Domain</th>
+                <th>IP</th>
+                <th>University</th>
+            </tr>
+            {
+              data.users.map((item,i)=>(
+              <tr key={i}>
+                  <td>{item.id}</td>
+                  <td><img src={item.image} alt="" /></td>
+                  <td>{item.firstName}</td>
+                  <td>{item.lastName}</td>
+                  <td>{item.gender}</td>
+                  <td>{item.email}</td>
+                  <td>{item.username}</td>
+                  <td>{item.domain}</td>
+                  <td>{item.ip}</td>
+                  <td>{item.university}</td>
+              </tr>
+            ))}
+        </thead>
+        </table>
     </div>
-  );
+  )
 }
 
 export default App;
